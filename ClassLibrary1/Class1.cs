26983fdd3f034aca13ffbd92e90e3325c7f7b9ea
sskr <samashivakumarreddy@gmail.com>
2016-08-16 22:30:01 -0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+
+        }
+        public static int sub(int a, int b)
+        {
+            int c = a - b;
+            return c;
+
+        }
+        public static int per(int a, int b)
+        {
+            int c = a % b;
+            return c;
+
+        }
+        public int mult(int a, int b)
+        {
+            int c = a * b;
+            return c;
+
+        }
+        
+
+    }
+}
